95ca1ed032ab0e64bdbd0eab232c89bd868f37df
bijukutta <42510656+bijukutta@users.noreply.github.com>
2018-08-27 13:18:09 +0530

    s

--- src/lu/lllc/ListProducts.java
+++ src/lu/lllc/ListProducts.java
@@ -48,40 +48,18 @@ public class ListProducts extends HttpServlet {
 		String dbURL = DBInfo.getDBURL();
 		String user = DBInfo.getUser();
 		String password = DBInfo.getPassword();
-
-		try {
-
+                
+                
+                
 			Class.forName(DBInfo.getDriver());
-		} catch (ClassNotFoundException e) {
-			System.out.println("Error. Driver class not found: " + e);
-		}
-
-		try {
 			connection = DriverManager.getConnection(dbURL, user, password);
-		} catch (SQLException e) {
-			System.out.println("Error. Connection problem: " + e);
-			return;
-		}
-
-		try {
 			statement = connection.createStatement();
-		} catch (SQLException e) {
-			System.out.println("Error. Can not create the statement: " + e);
-			return;
-		}
-
 		String searchString = "SELECT * FROM products";
-		try {
 			result = statement.executeQuery(searchString);
-		} catch (SQLException e) {
-			System.out.println("Error. Problem with executeUpdate: " + e);
-			return;
-		}
 
 		// Now we collect the data from the result in order to display them in
 		// JSP
 
-		try {
 			while (result.next()) {
 				Book book = new Book();
 				String title = result.getString("title");
@@ -94,17 +72,7 @@ public class ListProducts extends HttpServlet {
 				book.setPrice(price);
 				bookList.add(book);
 			}
-		} catch (SQLException e) {
-			System.out.println("Error. Problem reading data: " + e);
-			return;
-		}
-
-		try {
 			connection.close();
-		} catch (SQLException e) {
-			System.out.println("Error. Problem with closing connection: " + e);
-			return;
-		}
 
 		RequestDispatcher disp = request
 				.getRequestDispatcher("/WEB-INF/showBookList.jsp");
--- src/lu/lllc/Openshift DBInfo.java
+++ src/lu/lllc/Openshift DBInfo.java
@@ -1,20 +1,18 @@
 package lu.lllc;
 
 public class DBInfo {
-	//static String host = "jws-app-mysql"; // The standard name in Tomcat+ MySQL on openshift
-	static String host = "localhost"; // The standard name in Pc Server
+	static String host = "jws-app-mysql"; // The standard name in Tomcat+ MySQL on openshift
 
 	static String DBname = "llc";
 	static int port = 3306; // This is for MySQL
 	static String mySQLdbURL = "jdbc:mysql://" + host + ":" + port + "/"
 			+ DBname;
 
-	static String user = "root";
-	static String password = "root";
-//	static String user = "user";
-//	static String password = "password";
+	static String user = "user";
+	static String password = "password";
 	
 	static String driver = "com.mysql.jdbc.Driver";
+
 	public DBInfo() {
 
 	}
